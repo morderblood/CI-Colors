2113d492292af26b6d53985965ae938da127edc4
Vlad <kravavlad2410@gmail.com>
2025-12-06 00:08:04 +0100

    generating samples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,12 +6,22 @@ fun main() {
     val step = 0.25
     val algo = "NSGAII"
 
-    val outputPath = generateOutputPath(algo, numColors, step, "results")
+    val outputPath = generateOutputPath(algo, numColors, step, "datasets")
 
     val samplesGenerator = SamplesGenerator()
     samplesGenerator.generateTrainingDataset(outputPath, numColors, step)
 
-
+    samplesGenerator.generateOptimizationSamples(
+        trainingDataPath = outputPath,
+        resultOutputPath = generateOutputPath(algo, numColors, step, "results"),
+        optimizerName = algo,
+        errorName = "DeltaE2000",
+        includeSparsityPenalty = true,
+        useOptimizerFactoryDefaults = true,
+        initialGuessType = "Uniform",
+        optimizationParameters = emptyMap(),
+        numberOfSamples = 10
+    )
 }
 fun generateOutputPath(algo: String, numColors: Int, step: Double, dataType: String): String {
     return "C:\\Users\\safii\\IdeaProjects\\CI-Colors\\src\\main\\kotlin\\datasets\\$dataType-$algo-$numColors-colors-$step-step.csv"
