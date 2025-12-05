e12ee3e12695ceaa5d582b880ec26acf6c5f4955
Vlad <kravavlad2410@gmail.com>
2025-12-05 23:23:01 +0100

    Everything broken in IDE

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,6 @@ dependencies {
 
     // Apache Commons Math for optimization algorithms
     implementation("org.apache.commons:commons-math3:3.6.1")
-
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    implementation("org.moeaframework:moeaframework:5.1")
 }
 
 tasks.test {
--- src/main/kotlin/examples/Main.kt
+++ src/main/kotlin/examples/Main.kt
@@ -1,44 +1,20 @@
 package examples
 
-import data.Palette
-import evaluation.DataSetItemReader
-import evaluation.DataSetItemWriter
-import evaluation.OptimizationResultData
-import functional.error.MixingErrorFactory
-import functional.initialGuess.InitialGuessGeneratorFactory
-import functional.mixer.MixboxColorMixer
-import functional.normalizer.ProportionsNormalizer
-import goal.Goal
-import optimizer.OptimizerFactory
-import penalty.Penalty
-import penalty.SparsityPenalty
+class Main {
+    fun main() {
 
-fun main() {
+        val numColors = 3
+        val step = 0.25
+        val algo = "NSGAII"
 
-    val numColors = 3
-    val step = 0.25
+        val outputPath = generateOutputPath(algo, numColors, step, "results")
 
-    val trainingDataPath = "C:\\Users\\safii\\IdeaProjects\\CI-Colors\\src\\main\\kotlin\\datasets\\training_set_$numColors-colors-$step-step_for_nsgaII.csv"
+        val samplesGenerator = SamplesGenerator()
+        samplesGenerator.generateTrainingDataset(outputPath, numColors, step)
 
-    GenerateTrainingSet.generateDataset(trainingDataPath, numColors, step)
 
-    val algos = arrayOf("BOBYQA", "SMSEMOA", "Nelder-Mead", "Powell", "NSGAII", "CMA-ES")
-
-    val samplesGenerator = SamplesGenerator()
-    for (algo in algos) {
-        println("Generating results for $algo...")
-        val resultOutputPath = generateOutputPath(algo, numColors, step, "result")
-        samplesGenerator.generateOptimizationSamples(
-            trainingDataPath,
-            resultOutputPath,
-            algo,
-            "DeltaE2000",
-            true,
-            numberOfSamples = 1,
-            useOptimizerFactoryDefaults = true,
-            optimizationParameters = mapOf("populationSize" to 100))
     }
-}
-fun generateOutputPath(algo: String, numColors: Int, step: Double, dataType: String): String {
-    return "C:\\Users\\safii\\IdeaProjects\\CI-Colors\\src\\main\\kotlin\\datasets\\$dataType-$algo-$numColors-colors-$step-step.csv"
+    fun generateOutputPath(algo: String, numColors: Int, step: Double, dataType: String): String {
+        return "C:\\Users\\safii\\IdeaProjects\\CI-Colors\\src\\main\\kotlin\\datasets\\$dataType-$algo-$numColors-colors-$step-step.csv"
+    }
 }
